15e4e9c13d07d58179144f050b1f23420b91c23d
maxiao <maxiao@ywwl.com>
2021-03-08 21:19:09 +0800

    添加一些注释

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask
-
 from com import taskSDK,task
 
+#创建一个app
 app = Flask(__name__)
 
+#引用调度器
 scheduler = task.scheduler
+#将调度器加载到应用中
 scheduler.init_app(app)
+#启动调度器
 scheduler.start()
 
-app.register_blueprint(taskSDK.taskSDK)
+#在应用中注册task蓝图对象
 app.register_blueprint(task.task)
+#在应用中注册taskSDK蓝图对象
+app.register_blueprint(taskSDK.taskSDK)
+
 
 
 
--- com/task.py
+++ com/task.py
@@ -3,7 +3,10 @@ import datetime, pickle
 from flask_apscheduler import APScheduler
 from com.taskDemo import taskDemo
 
+#创建一个调度器
 scheduler = APScheduler()
+
+#创建一个task蓝图对象
 task = Blueprint("task", __name__)
 
 def task_print_date():
@@ -22,31 +25,31 @@ def task1():
     return "打印当前时间"
 
 @task.route('/pause', methods=['GET'])
-def pause_job():
+def pause_job():#暂停任务
     job_id = "循环任务"
     scheduler.pause_job(str(job_id))
     return '暂停成功'
 
 @task.route('/get_jobs', methods=['GET'])
-def get_task():
+def get_task():#获取任务列表
     jobs = scheduler.get_jobs()
     print(jobs)
     return 'jobs:' + str(pickle.dumps(jobs))
 
 @task.route('/get_job', methods=['GET'])
-def get_task_detail():
+def get_task_detail():#获取任务详情
     job = scheduler.get_job(id='循环任务')
     print(job)
     return 'job:' + str(pickle.dumps(job))
 
 @task.route('/resume', methods=['GET'])
-def resume_job():
+def resume_job():#恢复被暂停的任务
     job_id = "循环任务"
     scheduler.resume_job(str(job_id))
     return 'Success!'
 
 @task.route('/remove_job', methods=['GET'])
-def remove_job():
+def remove_job():#删除任务
     job_id = '循环任务'
     scheduler.remove_job(str(job_id))
     return 'remove success!'
