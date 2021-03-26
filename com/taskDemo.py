f0226798a29747652336adeeaa223ac573a096f2
maxiao <maxiao@ywwl.com>
2021-03-27 02:49:35 +0800

    将异步执行步骤放到SDK中

--- com/taskDemo.py
+++ com/taskDemo.py
@@ -1,9 +1,7 @@
 import time,json
-from concurrent.futures import ThreadPoolExecutor
 from com.taskSDK import taskResult
 
-#创建线程池
-executor = ThreadPoolExecutor(1)
+
 
 class taskDemo():
     '''注意：此模块需要在项目中任意py文件from com.route.job.taskDemo import taskDemo，否则SDK动态动态初始化对象会报错'''
@@ -18,24 +16,53 @@ class taskDemo():
             "actionId": "23232323" #任务日志ID,自动生成传入
         }
         '''
-        #提交一个线程执行具体的项目函数
-        executor.submit(self.test1,**kwargs)
-        print("项目已开始执行，请等待结果")
-        #先返回正在执行的信息
-        return "项目已开始执行，请等待结果"
-
-    def test1(self,**kwargs):
-        '''
-            各自项目具体要执行的函数
-        '''
         actionId = kwargs['actionId']
         params = kwargs['params']
-        params = json.loads(params)
+        # params = json.loads(params)
         len = params['len']
-        time.sleep(5)
+        time.sleep(10)
         print("执行一次aaa" + len)
         result = "执行一次aaa"
-        #调用taskSDK的respResult方法，将执行结果传过去
+        # 调用taskSDK的respResult方法，将执行结果传过去
         sdk = taskResult()
-        ret = sdk.respResult(actionId,result)
+        ret = sdk.respResult(actionId, result)
         return "执行完毕"
+
+
+
+#
+#
+# class taskDemo():
+#     '''注意：此模块需要在项目中任意py文件from com.route.job.taskDemo import taskDemo，否则SDK动态动态初始化对象会报错'''
+#     def insideDemo(self,**kwargs):
+#         '''
+#         接入SDK的函数，在任务调度平台，保存以下信息：
+#         {
+#             "classfile": "com.route.job.taskDemo", #目录
+#             "className": "taskDemo", #类名
+#             "funcName": "insideDemo", #方法名
+#             "param": {"len": "10"} #方法传参
+#             "actionId": "23232323" #任务日志ID,自动生成传入
+#         }
+#         '''
+#         #提交一个线程执行具体的项目函数
+#         executor.submit(self.test1,**kwargs)
+#         print("项目已开始执行，请等待结果")
+#         #先返回正在执行的信息
+#         return "项目已开始执行，请等待结果"
+#
+#     def test1(self,**kwargs):
+#         '''
+#             各自项目具体要执行的函数
+#         '''
+#         actionId = kwargs['actionId']
+#         params = kwargs['params']
+#         params = json.loads(params)
+#         len = params['len']
+#         time.sleep(5)
+#         print("执行一次aaa" + len)
+#         result = "执行一次aaa"
+#         #调用taskSDK的respResult方法，将执行结果传过去
+#         sdk = taskResult()
+#         ret = sdk.respResult(actionId,result)
+#         return "执行完毕"
--- com/taskSDK.py
+++ com/taskSDK.py
@@ -1,7 +1,7 @@
 import sys,json,requests
 from past.builtins import apply
 from flask import request,Blueprint
-
+from concurrent.futures import ThreadPoolExecutor
 
 '''
 author: sunqin
@@ -12,18 +12,22 @@ author: sunqin
         "classfile": "com.ywwl.Services.faker.impl.fakerDataImpl", #目录
         "className": "fakerDataImpl", #类名
         "funcName": "getFakerPwd", #方法名
-        "param": {"len": "10"} #方法传参
+        "params": {"len": "10"} #方法传参
         "actionId": "23232323" #任务日志ID
     }
 '''
 
+#创建线程池
+executor = ThreadPoolExecutor(1)
+
+#创建蓝图对象
 taskSDK = Blueprint("taskSDK",__name__)
 @taskSDK.route('/recieveTask',methods=['GET','POST'])
 def recieveTask():
     try:
         data = request.get_json(force=True)
         ret = execTask(**data)
-        return json.dumps({'resultCode': 1, 'result': ret})
+        return json.dumps({'resultCode': 0, 'result': ret})
     except Exception as e:
         print(e)
         return json.dumps({'resultCode': 1, 'result': str(e)})
@@ -45,10 +49,17 @@ def execTask(**kwargs):
     aClass = getattr(aMod, className)
     a = aClass()
 
-    #动态执行方法
+    # #提交一个线程去动态执行方法
+    # func = a.__getattribute__(funcName)
+    # executor.submit(apply(func,**paramdict))
+    executor.submit(run,a,funcName,**paramdict)
+    print('项目已开始执行，请等待结果')
+    return '项目已开始执行，请等待结果'
+
+def run(a,funcName,**paramdict):
+    # 提交一个线程去动态执行方法
     func = a.__getattribute__(funcName)
-    ret = apply(func,**paramdict)
-    return ret
+    executor.submit(apply(func, **paramdict))
 
 class taskResult():
     def respResult(self,actionId,result):
